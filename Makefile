515465a9cf37c9eeccf7044b3199bea07df0edb9
Денис Проскуряков <idarhani3567@gmail.com>
2025-08-16 20:33:10 +0600

    fix

--- Makefile
+++ Makefile
@@ -8,3 +8,5 @@ package-install:
 		uv tool install dist/*.whl
 lint:
 		uv run ruff check brain_games
+fix:
+		uv run ruff check --fix
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from brain_games.cli import welcom_user
 
 def main():
     print("Welcome to the Brain Games!")
     welcome_user()
+
 
 if __name__ == "_main_":
     main()
