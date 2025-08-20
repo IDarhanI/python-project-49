b8951d3c4bf61af66de12a0c534f3d5fc56fad06
Денис Проскуряков <idarhani3567@gmail.com>
2025-08-20 17:58:43 +0600

    fix

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -7,14 +7,14 @@ RULES = "What number is missing in the progression?"
 def generate_progression(start, step, length):
     progression = []
     for index in range(length):
-        current_element = start + index + step
+        current_element = start + index * step
         progression.append(str(current_element))
     return progression
 
 
 def generate_round():
     start = randint(1, 20)
-    step = randint(2, 10)
+    step = randint(1, 10)
     length = randint (5, 10)
 
 
