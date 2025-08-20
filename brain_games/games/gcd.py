8f3b7565c7acad5e5b89d02e04b6a368a5a1d950
Денис Проскуряков <idarhani3567@gmail.com>
2025-08-20 16:59:05 +0600

    add gcd

---
+++ brain_games/games/gcd.py
@@ -0,0 +1,18 @@
+from random import randint
+
+
+RULES = "Find the greatest common divisor of given numbers."
+
+
+def calculeate_gcd(a, b):
+    while b != 0:
+        a, b = b, a % b 
+    return a
+
+
+def generate_round():
+    a = randint(1, 100)
+    b = randint(1, 100)
+    question = f"{a} {b}"
+    correct_answer = calculeate_gcd(a, b)
+    return question, str(correct_answer)
