1d9653ed40fcc097771292c58ef51d981bcb75a9
Денис Проскуряков <idarhani3567@gmail.com>
2025-08-20 17:41:52 +0600

    progression buil 0.1

---
+++ brain_games/games/progression.py
@@ -0,0 +1,31 @@
+from random import randint, choice
+
+
+RULES = "What number is missing in the progression?"
+
+
+def generate_progression(start, step, length):
+    progression = []
+    for index in range(length):
+        current_element = start + index + step
+        progression.append(str(current_element))
+    return progression
+
+
+def generate_round():
+    start = randint(1, 20)
+    step = randint(1, 10)
+    length = randint (5, 10)
+
+
+    progression = generate_progression(start, step, lenght)
+
+
+    hidden-index = randint(0, length - 1)
+    correct_answer = progression[hidden_index]
+
+    progression[hidden_index] = ".."
+    question = " ".join(progression)
+
+
+    return question, correct_answer
