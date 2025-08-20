3d5297f3c2daa9d5f4bdfc02b0776015e6b6736e
Денис Проскуряков <idarhani3567@gmail.com>
2025-08-20 18:26:35 +0600

    add game prime

---
+++ brain_games/games/prime.py
@@ -0,0 +1,18 @@
+from random import randint
+
+RULES = 'Aswer "yes" if given number is prime. Otherwise answer "no".'
+
+
+def is_prime(number):
+    if number < 2:
+        return False
+    if number == 2:
+        return True
+    if number % 2 == 0:
+        return False
+    
+
+    for divisor in range(3, int(number ** 0.5) + 1, 2):
+        if number % divisor == 0:
+            return False
+    return True
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,10 @@
+from brain_games.engine import run_engine
+from brain_games.games import prime
+
+
+def main():
+    run_engine(gcd)
+
+
+if __name__ == "__main__":
+    main()
